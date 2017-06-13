4b6b3b3bed9390fa71cbfaf38d2ebcdf355fc685
Alexander <alex45510@ya.ru>
2017-06-14 00:46:17 +0400

    bot updated

--- bot.py
+++ bot.py
@@ -1,12 +1,18 @@
 # coding: utf-8
 import telebot
 from config import token
+from functools import wraps
 bot = telebot.TeleBot(token)
+
 
 @bot.message_handler(commands=['start'])
 def start_handler(message):
     text = 'Дратути'
     bot.send_message(message.chat.id, text)
+
+@bot.message_handler(commands=['leha'])
+def leha_handler(message):
+    bot.send_message(message.chat.id, 'GOVNA LEPEHA')
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
@@ -14,7 +20,10 @@ def echo_all(message):
         'калий': 'литий',
         'литий': 'гелий',
         'гелий': 'калий',
-        'лёха': 'говна лепёха'
+        'лёха': 'говна лепёха',
+        'вышка': 'листва',
+        'листочек': 'дай мефчик',
+        'сосна': 'хуй на!'
     }
     text = message.text.lower()
     for word, answer in answers_dict.items():
